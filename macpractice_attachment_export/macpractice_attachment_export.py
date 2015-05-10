c24d5d9ce14e2e801638b416f3b9652e4cf3b865
Colin Johnson <colinbjohnson@gmail.com>
2015-05-10 20:37:58 +0000

    use Attachemnt, Patient and Person objects

--- macpractice_attachment_export/macpractice_attachment_export.py
+++ macpractice_attachment_export/macpractice_attachment_export.py
@@ -7,138 +7,19 @@ import shutil
 
 import mysql.connector
 
+import sys
+sys.path.append("../mptools")
+from mptools import Attachment, Patient, Person
 
-def get_row_result(query):
+
+def export_attachment(attachment, target_dir, file_name):
     '''
-    given a MySQL Query, return the result or return None.
+    given an attachment object, a target_dir and a file_name,
+    copy the given attachment into that specified target_dir
     '''
-    cursor = mysql_connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print query
-    if result and len(result) == 1:
-        return result[0]
-    else:
-        return None
-
-
-def get_attachment_type(mysql_connection, attachment_id):
-    '''
-    given MySQL Connection Object and an Attachment ID, return the type of attachment
-    '''
-    attachment_type = None
-    attachment_type_query = ('SELECT attached_file.attached_file_id, image_attachments.attached_file_id, '
-                             'order_attachment_tie.attached_file_id '
-                             'FROM attached_file '
-                             'LEFT JOIN image_attachments '
-                             'ON attached_file.attached_file_id=image_attachments.attached_file_id '
-                             'LEFT JOIN order_attachment_tie '
-                             'ON attached_file.attached_file_id=order_attachment_tie.attached_file_id '
-                             'where attached_file.attached_file_id=\'{!s}\';'.format(attachment_id))
-    attachment_type_result = get_row_result(attachment_type_query)
-    if attachment_type_result is None:
-        logging.warn('attachment_id {!s} is of Type: Unknown.'.format(attachment_id))
-    else:
-        if attachment_type_result[1] == attachment_id:
-            attachment_type = 'image'
-            logging.info('attachment_id {!s} is of Type: Image.'.format(attachment_id))
-        elif attachment_type_result[2] == attachment_id:
-            attachment_type = 'order'
-            logging.info('attachment_id {!s} is of Type: Order.'.format(attachment_id))
-        else:
-            logging.warn('attachment_id {!s} is of Type: Unknown.'.format(attachment_id, attachment_type))
-    return attachment_type
-
-
-def get_image_attachment_row(mysql_connection, attached_file_id):
-    '''
-    given an attached_file_id return a patient_id
-    '''
-    image_attachments_row = get_row_result('select attached_file_id, patient_id from image_attachments where '
-                                           'attached_file_id=\'{!s}\';'.format(attached_file_id))
-    return image_attachments_row
-
-
-def get_order_patient_row(mysql_connection, attached_file_id):
-    '''
-    given an attached_file_id return a patient_id
-    '''
-    order_patient_row = get_row_result('SELECT order_attachment_tie.attached_file_id, '
-                                       'order_attachment_tie.orders_id, orders.patient_id '
-                                       'FROM order_attachment_tie '
-                                       'LEFT JOIN orders '
-                                       'ON order_attachment_tie.orders_id=orders.orders_id '
-                                       'WHERE order_attachment_tie.orders_id=\'{!s}\';')
-    return order_patient_row
-
-
-def get_attachment_metadata(mysql_connection, attachment_hash):
-    '''
-    given a mysql_connection and an attachment_hash return attachment data.
-    '''
-    attachment_metadata = {'attachment_hash': attachment_hash, 'attached_file_id': None, 'file_name': None,
-                           'patient_id': None, 'person_id': None,
-                           'last_name': None, 'first_name': None, 'attachment_type': None}
-    # lookup of attachment_hash
-    attached_file_row = get_row_result('select attached_file_id, file_name from attached_file where hash=\'{!s}\''
-                                       .format(attachment_hash))
-    if attached_file_row is not None:
-        attachment_metadata['attached_file_id'] = attached_file_row[0]
-        attachment_metadata['file_name'] = attached_file_row[1]
-    else:
-        logging.warn('attachment_hash {!s} does not exist in attached_file table.'.format(attachment_hash))
-
-    attachment_metadata['attachment_type'] = get_attachment_type(mysql_connection,
-                                                                 attachment_metadata['attached_file_id'])
-
-    if attachment_metadata['attached_file_id'] is not None:
-        if attachment_metadata['attachment_type'] == 'image':
-            image_attachments_row = get_image_attachment_row(mysql_connection, attachment_metadata['attached_file_id'])
-            if image_attachments_row is not None:
-                attachment_metadata['patient_id'] = image_attachments_row[1]
-            else:
-                logging.warn('attached_file_id {!s} does not exist in image_attachments table.'
-                             .format(attachment_metadata['attached_file_id']))
-        elif attachment_metadata['attachment_type'] == 'order':
-            order_patient_row = get_order_patient_row(mysql_connection, attachment_metadata['attached_file_id'])
-            if order_patient_row is not None:
-                attachment_metadata['patient_id'] = order_patient_row[3]
-            else:
-                logging.warn('attached_file_id {!s} does not exist in orders table.'
-                             .format(attachment_metadata['attached_file_id']))
-        else:
-            logging.warn('attachment_data is not found')
-
-    patient_person_row = get_row_result('SELECT patient.patient_id, patient.person_id, person.last, person.first '
-                                        'FROM patient '
-                                        'LEFT JOIN person '
-                                        'ON patient.person_id=person.person_id '
-                                        'WHERE patient.patient_id=\'{!s}\';'
-                                        .format(attachment_metadata['patient_id']))
-    if patient_person_row is not None:
-        attachment_metadata['person_id'] = patient_person_row[1]
-        attachment_metadata['last_name'] = patient_person_row[2]
-        attachment_metadata['first_name'] = patient_person_row[3]
-    else:
-        logging.warn('person_id {!s} does not exist in person table.'.format(attachment_metadata['person_id']))
-    print attachment_metadata
-    return attachment_metadata
-
-
-def process_attachment(attachment_path, attachment_metadata, target_dir):
-    '''
-    given: and attachment_path, attachment_metadata and a target_dir,
-    creates directory for attachment and copies attachment into directory and uses 'file_name' for file name
-    '''
-    logging.info('Processing Attachment: {!s}'.format(attachment_path))
-
-    attachment_uid = "{!s}-{!s}-{!s}-{!s}".format(attachment_metadata['last_name'],
-                                                  attachment_metadata['first_name'],
-                                                  attachment_metadata['person_id'],
-                                                  attachment_metadata['file_name'])
-    target_file = os.path.join(target_dir, attachment_uid)
-    shutil.copy(attachment_path, target_file)
-
+    target_file = os.path.join(target_dir, file_name)
+    shutil.copy(attachment.file_path, target_file)
+    return True
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--server', default='127.0.0.1',
@@ -159,8 +40,6 @@ args = parser.parse_args()
 log_format = '%(message)s'
 logging.basicConfig(level='INFO', format=log_format)
 
-# creates a list of all files to be converted
-files = os.walk(args.source_dir)
 
 mysql_connection = mysql.connector.connect(user=args.username,
                                            password=args.password,
@@ -168,12 +47,36 @@ mysql_connection = mysql.connector.connect(user=args.username,
                                            database=args.database,
                                            buffered=True)
 
-for root, directories, filenames in files:
-    for filename in filenames:
-        attachment_path = os.path.join(root, filename)
-        logging.info('Examining File: {!s}'.format(attachment_path))
-        # gets attachment metadata
-        attachment_metadata = get_attachment_metadata(mysql_connection, filename)
-        # moves attachment to correct directory
-        if (attachment_metadata['file_name'] is not None) and (attachment_metadata['file_name'] is not None):
-            process_attachment_result = process_attachment(attachment_path, attachment_metadata, args.target_dir)
+attachments = {}
+# create dictionary of all Patients
+patients = Patient.get_all_patients(mysql_connection)
+# for each Patient, get last, first
+for patient_id in patients:
+    person = Person.get_person_by_id(mysql_connection, patients[patient_id].person_id)
+    patients[patient_id].last = person.last
+    patients[patient_id].first = person.first
+
+# creates a list of all files to be exported from MacPractice
+files = os.walk(args.source_dir)
+for root, directories, file_names in files:
+    for file_name in file_names:
+        file_path = os.path.join(root, file_name)
+        logging.info('Examining File: {!s}'.format(file_path))
+        attachment = Attachment.get_attachment_by_hash(mysql_connection, file_name)
+        if attachment is not None:
+            attachment_id = attachment.attached_file_id
+            attachment.file_path = file_path
+            attachment.attachment_type = attachment.get_attachment_type(mysql_connection)
+            attachment.patient_id = attachment.get_patient_id(mysql_connection, attachment.attachment_type)
+            attachments[attachment_id] = attachment
+
+for attachment_id in attachments:
+    attachment = attachments[attachment_id]
+    last = None
+    first = None
+    if attachment.patient_id is not None and patients[attachment.patient_id] is not None:
+        last = patients[attachment.patient_id].last
+        first = patients[attachment.patient_id].first
+    logging.info('Preparing to Export Attachment: {!s}'.format(attachment.attached_file_id))
+    file_name = "{!s}-{!s}-{!s}".format(last, first, attachment.file_name)
+    export_result = export_attachment(attachments[attachment_id], args.target_dir, file_name)
---
+++ mptools/mptools.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+
+import logging
+
+import mysql.connector
+
+
+class Attachment:
+    def __init__(self, attached_file_id):
+        self.attached_file_id = attached_file_id
+        self.hash = None
+        self.file_name = None
+        # all attributes below are extensions to the Attachment class
+        self.patient_id = None
+        self.attachment_type = None
+        self.file_path = None
+
+    def get_attachment_type(self, mysql_connection):
+        # TODO: clean up attachment_type_query
+        '''
+        given MySQL Connection Object and an Attachment ID, return the type of attachment
+        '''
+        attachment_type = None
+        attachment_type_query = ('SELECT attached_file.attached_file_id AS attached_file_id, '
+                                 'image_attachments.attached_file_id AS attached_file_id_image, '
+                                 'order_attachment_tie.attached_file_id AS attached_file_id_order '
+                                 'FROM attached_file '
+                                 'LEFT JOIN image_attachments '
+                                 'ON attached_file.attached_file_id=image_attachments.attached_file_id '
+                                 'LEFT JOIN order_attachment_tie '
+                                 'ON attached_file.attached_file_id=order_attachment_tie.attached_file_id '
+                                 'where attached_file.attached_file_id=\'{!s}\';'.format(self.attached_file_id))
+        attachment_type_result = get_row_result(mysql_connection, attachment_type_query)
+        if attachment_type_result is None:
+            logging.warn('attachment_id {!s} is of Type: Unknown.'.format(self.attached_file_id))
+        else:
+            if attachment_type_result['attached_file_id_image'] == self.attached_file_id:
+                attachment_type = 'image'
+                logging.info('attachment_id {!s} is of Type: Image.'.format(self.attached_file_id))
+            elif attachment_type_result['attached_file_id_order'] == self.attached_file_id:
+                attachment_type = 'order'
+                logging.info('attachment_id {!s} is of Type: Order.'.format(self.attached_file_id))
+            else:
+                logging.warn('attachment_id {!s} is of Type: Unknown.'.format(self.attached_file_id))
+        return attachment_type
+
+    def get_patient_id(self, mysql_connection, attachment_type):
+        patient_id = None
+        if attachment_type == 'image':
+            query = 'SELECT patient_id FROM image_attachments WHERE ' \
+                    'attached_file_id=\'{!s}\';'.format(self.attached_file_id)
+        elif attachment_type == 'orders':
+            query = 'SELECT orders.patient_id FROM order_attachment_tie ' \
+                    'LEFT JOIN orders ON order_attachment_tie.orders_id=orders.orders_id ' \
+                    'WHERE order_attachment_tie.orders_id=\'{!s}\';'\
+                    .format(self.attached_file_id)
+        else:
+            return None
+        patient_id_row = get_row_result(mysql_connection, query)
+        patient_id = patient_id_row['patient_id']
+        return patient_id
+
+    @classmethod
+    def get_attachment_by_id(cls, mysql_connection, attachment_id):
+        attachment = None
+        query = 'SELECT attached_file_id, hash, file_name '\
+                'FROM attached_file WHERE attached_file_id=\'{!s}\';'.format(attachment_id)
+        attachment_row = get_row_result(mysql_connection, query)
+        if attachment_row is not None:
+            attached_file_id = attachment_row['attached_file_id']
+            attachment = Attachment(attached_file_id)
+            attachment.file_name = attachment_row['file_name']
+            attachment.hash = attachment_row['hash']
+        return attachment
+
+    @classmethod
+    def get_attachment_by_hash(cls, mysql_connection, attachment_hash):
+        attachment = None
+        query = 'SELECT * FROM attached_file WHERE hash=\'{!s}\';'.format(attachment_hash)
+        attachment_row = get_row_result(mysql_connection, query)
+        if attachment_row is not None:
+            logging.info('Attachment with Hash {!s} found.'.format(attachment_hash))
+            attached_file_id = attachment_row['attached_file_id']
+            attachment = Attachment(attached_file_id)
+            attachment.file_name = attachment_row['file_name']
+            attachment.hash = attachment_row['hash']
+        else:
+            logging.warn('Attachment with Hash {!s} not found.'.format(attachment_hash))
+        return attachment
+
+    @classmethod
+    def get_all_attachments(cls, mysql_connection):
+        attachments = {}
+        query = 'SELECT * FROM attached_file'
+        cursor = mysql_connection.cursor(dictionary=True)
+        cursor.execute(query)
+        all_attachment_rows = cursor.fetchall()
+        for attachment_row in all_attachment_rows:
+            attached_file_id = attachment_row['attached_file_id']
+            attachments[attached_file_id] = Attachment(attached_file_id)
+            attachments[attached_file_id].file_name = attachment_row['file_name']
+            attachments[attached_file_id].hash = attachment_row['hash']
+        return attachments
+
+
+class Patient:
+    def __init__(self, patient_id):
+        self.patient_id = patient_id
+        # all attributes below are extensions to the Attachment class
+        self.person_id = None
+        self.last = None
+        self.first = None
+
+    @classmethod
+    def get_all_patients(cls, mysql_connection):
+        patients = {}
+        query = 'SELECT * FROM patient'
+        cursor = mysql_connection.cursor(dictionary=True)
+        cursor.execute(query)
+        all_patient_rows = cursor.fetchall()
+        for patient_row in all_patient_rows:
+            patient_id = patient_row['patient_id']
+            patients[patient_id] = Patient(patient_id)
+            patients[patient_id].person_id = patient_row['person_id']
+        return patients
+
+
+class Person:
+    def __init__(self, person_id):
+        self.person_id = person_id
+        self.last_name = None
+        self.first_name = None
+
+    @classmethod
+    def get_person_by_id(cls, mysql_connection, person_id):
+        person = None
+        query = 'SELECT * FROM person WHERE person_id=\'{!s}\';'.format(person_id)
+        person_row = get_row_result(mysql_connection, query)
+        if person_row is not None:
+            person_id = person_row['person_id']
+            person = Person(person_id)
+            person.last = person_row['last']
+            person.first = person_row['first']
+        return person
+
+
+def get_row_result(mysql_connection, query):
+    '''
+    given a MySQL Query, return the result or return None.
+    '''
+    cursor = mysql_connection.cursor(dictionary=True)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    if result and len(result) == 1:
+        return result[0]
+    else:
+        return None
